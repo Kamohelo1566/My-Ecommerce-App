410eac04ef8fe2284f8d99d5d9ed1c2b5236ec0e
Kamohelo <kamoheloacc@gmail.com>
2026-03-03 12:03:52 +0200

    admin dashboard tools setup

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,7 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
+
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -11,14 +16,19 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
+      <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
       <App />
+      </QueryClientProvider>
       </ClerkProvider>
+      </BrowserRouter>
     </StrictMode>
   );
 }
